3af91efbc9e523c7f45fb0debc532448987cfc8d
THiragi <w1th1nmybr41n@gmail.com>
2023-04-13 15:19:05 +0900

    replace puppeteer to jsdom

--- pages/api/scraper/amazon.ts
+++ pages/api/scraper/amazon.ts
@@ -1,35 +1,27 @@
+import fetch from 'node-fetch'
+import { JSDOM } from 'jsdom'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import chrome from 'chrome-aws-lambda'
-import puppeteer from 'puppeteer-core'
-import type { Browser } from 'puppeteer-core'
 
-export async function getAmazonItem(browser: Browser, url: string) {
-  const page = await browser.newPage()
-  const id = url.replace(`https://www.amazon.co.jp/dp/`, '').split('?')[0]
-  const res = await page.goto(url, { waitUntil: 'domcontentloaded' })
-  if (res?.status() !== 200) {
-    return {
-      result: 'error',
-      id,
-      url,
-      title: '',
-      price: '',
-      timestamp: '0',
-    }
-  }
-  const title = await page.title()
-  const price = await page.evaluate(() => {
-    return document.getElementsByClassName('a-price-whole')[0].textContent
-  })
-  await page.close()
-  return {
-    result: 'success',
-    id,
-    url,
-    title,
-    price: price ?? '',
-    timestamp: new Date().toLocaleString('sv-SE'),
-  }
+export async function getAmazonItem(url: string) {
+  return await fetch(url)
+    .then((data) => data.text())
+    .then((text) => {
+      const {
+        window: { document },
+      } = new JSDOM(text)
+      const id = url.replace(`https://www.amazon.co.jp/dp/`, '').split('?')[0]
+      const title = document.title
+      const price =
+        document.getElementsByClassName('a-price-whole')[0].textContent
+      return {
+        result: 'success',
+        id,
+        url,
+        title,
+        price: price ?? '',
+        timestamp: new Date().toLocaleString('sv-SE'),
+      }
+    })
 }
 
 export type AmazonItem = Awaited<ReturnType<typeof getAmazonItem>>
@@ -53,17 +45,11 @@ export default async function handler(
       const filteredUrls = urls.filter((url) =>
         url.includes(`https://www.amazon.co.jp/dp/`)
       )
-      const browser = await puppeteer.launch({
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      })
 
       const promise = filteredUrls.map(async (url) => {
-        return getAmazonItem(browser, url)
+        return getAmazonItem(url)
       })
       const result = await Promise.all(promise)
-      browser.close()
 
       res.status(200).json({ result })
       break
