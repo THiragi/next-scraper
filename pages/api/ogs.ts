cd27c07e35a5e9fc18c238ea4547c1af7c686b59
THiragi <w1th1nmybr41n@gmail.com>
2023-08-09 23:54:59 +0900

    remove scraper

--- pages/api/ogs.ts
+++ pages/api/ogs.ts
@@ -1,15 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import openGraphScraper from 'open-graph-scraper'
+import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 function getDomainFromUrl(url: string) {
   const match = url.match(
     /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?=]+)/im
-  )
-  return match ? match[1] : undefined
+  );
+  return match ? match[1] : undefined;
 }
 
 async function getOgData(url: string) {
-  const options = { url, onlyGetOpenGraphInfo: true }
+  const options = { url, onlyGetOpenGraphInfo: true };
   return openGraphScraper(options).then((data) => {
     const {
       success,
@@ -19,18 +20,18 @@ async function getOgData(url: string) {
       ogSiteName,
       ogImage,
       twitterImage,
-    } = data.result
-    const domain = ogUrl ? getDomainFromUrl(ogUrl) : ''
+    } = data.result;
+    const domain = ogUrl ? getDomainFromUrl(ogUrl) : '';
     const image = Array.isArray(ogImage)
       ? ogImage[0]
       : typeof ogImage === 'string'
       ? undefined
-      : ogImage
+      : ogImage;
     const twitterOgImage = Array.isArray(twitterImage)
       ? twitterImage[0]
       : typeof twitterImage === 'string'
       ? undefined
-      : twitterImage
+      : twitterImage;
     return {
       success,
       url: ogUrl ?? url,
@@ -38,39 +39,39 @@ async function getOgData(url: string) {
       title: ogTitle ?? ogSiteName ?? domain,
       description: ogDescription ?? url,
       ogImageSrc: image?.url ?? twitterOgImage?.url,
-    }
-  })
+    };
+  });
 }
 
-type OgData = Awaited<ReturnType<typeof getOgData>>
+type OgData = Awaited<ReturnType<typeof getOgData>>;
 
 type Data = {
-  result: OgData[]
-}
+  result: OgData[];
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { method, headers, body } = req
+  const { method, headers, body } = req;
 
   switch (method) {
     case 'POST':
       if (headers['x-api-key'] !== process.env.API_KEY_OGS) {
-        res.status(404).end('invalid api key')
+        res.status(404).end('invalid api key');
       }
-      const urls: string[] = Array.isArray(body.urls) ? body.urls : []
+      const urls: string[] = Array.isArray(body.urls) ? body.urls : [];
 
       const result = await Promise.all(
         urls.map(async (url) => {
-          return getOgData(url)
+          return getOgData(url);
         })
-      )
-      res.setHeader('Cache-Control', 's-maxage=86400')
-      res.status(200).json({ result })
-      break
+      );
+      res.setHeader('Cache-Control', 's-maxage=86400');
+      res.status(200).json({ result });
+      break;
     default:
-      res.status(404).end('')
-      break
+      res.status(404).end('');
+      break;
   }
 }
