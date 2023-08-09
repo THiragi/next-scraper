9f93e03adc0be15bde55321a7d6200cd7f7236bd
THiragi <w1th1nmybr41n@gmail.com>
2023-08-10 00:22:18 +0900

    update

--- pages/api/ogs.ts
+++ pages/api/ogs.ts
@@ -2,76 +2,99 @@ import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-function getDomainFromUrl(url: string) {
-  const match = url.match(
-    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?=]+)/im
+const urls = {
+  amazon: 'https://www.amazon.co.jp/dp/',
+};
+
+function isStringArray(array: unknown): array is string[] {
+  return (
+    Array.isArray(array) && array.every((item) => typeof item === 'string')
   );
-  return match ? match[1] : undefined;
+}
+
+function getDomainFromUrl(url: string): string | null {
+  try {
+    const { hostname } = new URL(url);
+    return hostname.replace('www.', '');
+  } catch {
+    return null;
+  }
+}
+
+function getQuerySelector(
+  document: Document
+): (selector: string) => string | null {
+  return (selector: string) =>
+    document?.querySelector(selector)?.getAttribute('content') ?? null;
 }
 
 async function getOgData(url: string) {
-  const options = { url, onlyGetOpenGraphInfo: true };
-  return openGraphScraper(options).then((data) => {
-    const {
-      success,
-      ogUrl,
-      ogDescription,
-      ogTitle,
-      ogSiteName,
-      ogImage,
-      twitterImage,
-    } = data.result;
-    const domain = ogUrl ? getDomainFromUrl(ogUrl) : '';
-    const image = Array.isArray(ogImage)
-      ? ogImage[0]
-      : typeof ogImage === 'string'
-      ? undefined
-      : ogImage;
-    const twitterOgImage = Array.isArray(twitterImage)
-      ? twitterImage[0]
-      : typeof twitterImage === 'string'
-      ? undefined
-      : twitterImage;
-    return {
-      success,
-      url: ogUrl ?? url,
-      domain,
-      title: ogTitle ?? ogSiteName ?? domain,
-      description: ogDescription ?? url,
-      ogImageSrc: image?.url ?? twitterOgImage?.url,
-    };
-  });
+  return await fetch(url)
+    .then((response) => response.text())
+    .then((text) => {
+      const {
+        window: { document },
+      } = new JSDOM(text);
+      const getContent = getQuerySelector(document);
+      const ogTitle = getContent('meta[property="og:title"]');
+      const ogDescription = getContent('meta[property="og:description"]');
+      const ogImage = getContent('meta[property="og:image"]');
+      const ogUrl = getContent('meta[property="og:url"]');
+      const twitterTitle = getContent('meta[name="twitter:title"]');
+      const twitterDescription = getContent('meta[name="twitter:description"]');
+      const twitterImage = getContent('meta[name="twitter:image"]');
+      const title = document?.title ?? getContent('meta[name="title"]');
+      const description = getContent('meta[name="description"]');
+      const amazon = url.includes(urls.amazon)
+        ? {
+            id: url.replace(urls.amazon, '').split('?')[0],
+            price:
+              document?.getElementsByClassName('a-price-whole')[0]
+                .textContent ?? null,
+          }
+        : undefined;
+      return {
+        url: ogUrl ?? url,
+        timestamp: new Date().toLocaleString('sv-SE'),
+        domain: getDomainFromUrl(ogUrl ?? url),
+        title: title ?? ogTitle ?? twitterTitle,
+        description: description ?? twitterDescription ?? ogDescription,
+        image: twitterImage ?? ogImage,
+        amazon,
+      };
+    });
 }
-
-type OgData = Awaited<ReturnType<typeof getOgData>>;
-
-type Data = {
-  result: OgData[];
-};
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   const { method, headers, body } = req;
 
-  switch (method) {
-    case 'POST':
-      if (headers['x-api-key'] !== process.env.API_KEY_OGS) {
-        res.status(404).end('invalid api key');
-      }
-      const urls: string[] = Array.isArray(body.urls) ? body.urls : [];
+  if (
+    headers['Secret-Token'] !== process.env.SECRET_TOKEN ||
+    headers['content-type']?.includes('application/json') ||
+    method !== 'POST'
+  ) {
+    res.status(404).end('invalid request');
+  }
 
-      const result = await Promise.all(
-        urls.map(async (url) => {
-          return getOgData(url);
-        })
-      );
+  const urls: string[] = isStringArray(body.urls) ? body.urls : [];
+
+  await Promise.all(
+    urls.map(async (url) => {
+      return getOgData(url);
+    })
+  )
+    .then((data) => {
+      const result = {
+        siteOgps: data.filter((ogp) => !ogp.amazon),
+        amazonItems: data.filter((ogp) => ogp.amazon),
+      };
       res.setHeader('Cache-Control', 's-maxage=86400');
       res.status(200).json({ result });
-      break;
-    default:
-      res.status(404).end('');
-      break;
-  }
+    })
+    .catch(() => {
+      res.status(404).end('scraping failed');
+    });
 }
